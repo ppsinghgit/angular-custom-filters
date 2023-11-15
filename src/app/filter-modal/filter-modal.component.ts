1ee7c7bcffcc64247220b37c0742f413bf6fccc8
ppsinghgit <parminderpalsingh@live.com>
2023-11-15 23:27:53 +0530

    1 file modified

--- src/app/filter-modal/filter-modal.component.ts
+++ src/app/filter-modal/filter-modal.component.ts
@@ -31,6 +31,8 @@ export class FilterModalComponent implements OnInit {
   public filterModelInCaseCancel: FilterModel;
   public selectedSectionType: string = '';
 
+  private enrollmentFilter = new EnrollmentFilter();
+
   constructor(public activeModal: NgbActiveModal) {
     this.selectedFilters = [];
   }
@@ -47,18 +49,76 @@ export class FilterModalComponent implements OnInit {
   selectField(type: string) {}
 
   changeSelection(val: string) {
+    this.addText = 'test';
     this.selectedSection = val;
     if (val == sections.view) {
+      this.addText = this.enrollmentFilter.view;
+    } else if (val == sections.year) {
+      this.addText = this.enrollmentFilter.year;
+    } else if (val == sections.enrollmentYear) {
+      this.addText = this.enrollmentFilter.enrollmentYear;
+    } else if (val == sections.programName) {
+      this.addText = this.enrollmentFilter.programName;
+    } else if (val == sections.contractNumber) {
+      this.addText = this.enrollmentFilter.contractNumber;
+    } else if (val == sections.distrbutorName) {
+      this.addText = this.enrollmentFilter.distrbutorName;
+    } else if (val == sections.enrolledBy) {
+      this.addText = this.enrollmentFilter.enrolledBy;
+    } else if (val == sections.enrollmentId) {
+      this.addText = this.enrollmentFilter.enrollmentId;
+    } else if (val == sections.enrollmentStatus) {
+      this.addText = this.enrollmentFilter.enrollmentStatus;
+    } else if (val == sections.payToName) {
+      this.addText = this.enrollmentFilter.payToName;
+    } else if (val == sections.payToAddress) {
+      this.addText = this.enrollmentFilter.payToAddress;
+    } else if (val == sections.payToCity) {
+      this.addText = this.enrollmentFilter.payToCity;
+    } else if (val == sections.payToState) {
+      this.addText = this.enrollmentFilter.payToState;
+    } else if (val == sections.payToZip) {
+      this.addText = this.enrollmentFilter.payToZip;
     }
   }
 
   addFilter() {
+    if (this.selectedSection == sections.view) {
+      this.enrollmentFilter.view = this.addText;
+    } else if (this.selectedSection == sections.year) {
+      this.enrollmentFilter.year = this.addText;
+    } else if (this.selectedSection == sections.enrollmentYear) {
+      this.enrollmentFilter.enrollmentYear = this.addText;
+    } else if (this.selectedSection == sections.programName) {
+      this.enrollmentFilter.programName = this.addText;
+    } else if (this.selectedSection == sections.contractNumber) {
+      this.enrollmentFilter.contractNumber = this.addText;
+    } else if (this.selectedSection == sections.distrbutorName) {
+      this.enrollmentFilter.distrbutorName = this.addText;
+    } else if (this.selectedSection == sections.enrolledBy) {
+      this.enrollmentFilter.enrolledBy = this.addText;
+    } else if (this.selectedSection == sections.enrollmentId) {
+      this.enrollmentFilter.enrollmentId = this.addText;
+    } else if (this.selectedSection == sections.enrollmentStatus) {
+      this.enrollmentFilter.enrollmentStatus = this.addText;
+    } else if (this.selectedSection == sections.payToName) {
+      this.enrollmentFilter.payToName = this.addText;
+    } else if (this.selectedSection == sections.payToAddress) {
+      this.enrollmentFilter.payToAddress = this.addText;
+    } else if (this.selectedSection == sections.payToCity) {
+      this.enrollmentFilter.payToCity = this.addText;
+    } else if (this.selectedSection == sections.payToState) {
+      this.enrollmentFilter.payToState = this.addText;
+    } else if (this.selectedSection == sections.payToZip) {
+      this.enrollmentFilter.payToZip = this.addText;
+    }
     let item = new SelectedFilterItem();
     item.Value = this.selectedSection + ' - ' + this.addText;
     item.Type = this.selectedSection;
     this.selectedFilters.push(item);
     this.addText = '';
     this.setFiltersCount();
+    console.log('test');
   }
 
   private setPrimaryData(data: Dropdowndata[]) {
@@ -66,8 +126,7 @@ export class FilterModalComponent implements OnInit {
     this.getPrimaryDataHeight();
   }
 
-  public validate() {
-  }
+  public validate() {}
 
   onSearchChange(searchValue: string): void {
     this.getPrimaryDataHeight();
@@ -79,7 +138,7 @@ export class FilterModalComponent implements OnInit {
 
   public radioButtonEvent(selected: boolean, value: string) {
     let item: any = null;
-  
+
     this.setFiltersCount();
   }
 
@@ -172,4 +231,21 @@ enum sectionType {
   SalesHierarchy = 'SalesHierarchy',
   AccountHierarchy = 'AccountHierarchy',
   RetailerAccounts = 'RetailerAccounts',
+}
+
+export class EnrollmentFilter {
+  view: string;
+  year: string;
+  enrollmentYear: string;
+  programName: string;
+  contractNumber: string;
+  distrbutorName: string;
+  enrolledBy: string;
+  enrollmentId: string;
+  enrollmentStatus: string;
+  payToName: string;
+  payToAddress: string;
+  payToCity: string;
+  payToState: string;
+  payToZip: string;
 }
